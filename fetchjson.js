6cdfd9581137848a3cf63e5db249004ee0076b81
trtle <77798301+trt1e@users.noreply.github.com>
2025-09-16 14:59:07 +0000

    add articels infrastructur

---
+++ fetchjson.js
@@ -0,0 +1,17 @@
+fetch("/articles.json")
+    .then(response => response.json())
+    .then(data => {
+        // 'data' is now an array of your article objects
+        const mostRecentArticle = data[0]; // The first item is the most recent
+        
+        // Find the element on your page where you want to display the article
+        const articleContainer = document.getElementById("article-container");
+        
+        // Populate the HTML with the data from the JSON file
+        articleContainer.innerHTML = `
+            <h2>${mostRecentArticle.title}</h2>
+            <img src="${mostRecentArticle.image}" alt="${mostRecentArticle.title}">
+            <p>${mostRecentArticle.content}</p>
+        `;
+    })
+    .catch(error => console.error("Could not fetch articles:", error));
