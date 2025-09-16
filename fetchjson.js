362403e37ec6a84ffd3d31a7aa801dacf99c5763
trtle <77798301+trt1e@users.noreply.github.com>
2025-09-16 15:03:50 +0000

    Fix fetch function initialization and update article container in fetchjson.js and index.html

--- fetchjson.js
+++ fetchjson.js
@@ -1,11 +1,17 @@
-fetch("/articles.json")
+window.onload() = function {
+
+}
+
+
+function loadTestFunction() {
+    fetch("./articles.json")
     .then(response => response.json())
     .then(data => {
         // 'data' is now an array of your article objects
         const mostRecentArticle = data[0]; // The first item is the most recent
         
         // Find the element on your page where you want to display the article
-        const articleContainer = document.getElementById("article-container");
+        const articleContainer = document.getElementById("article");
         
         // Populate the HTML with the data from the JSON file
         articleContainer.innerHTML = `
@@ -14,4 +20,8 @@ fetch("/articles.json")
             <p>${mostRecentArticle.content}</p>
         `;
     })
-    .catch(error => console.error("Could not fetch articles:", error));
+    .catch(
+        error => console.error("Could not fetch articles:", error),
+        alert(e)
+    );
+}
