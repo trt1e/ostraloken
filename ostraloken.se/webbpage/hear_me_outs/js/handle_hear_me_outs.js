0894dd2de011eaf6c1eeb7ac8f4e6d05c7f5be32
trt1e <vilhelm.grill@gmail.com>
2026-06-02 22:00:10 +0200

    made hear me out js work even if number of hear me outs are updated!

--- ostraloken.se/webbpage/hear_me_outs/js/handle_hear_me_outs.js
+++ ostraloken.se/webbpage/hear_me_outs/js/handle_hear_me_outs.js
@@ -6,34 +6,40 @@ Just nu fungerar så länge man inte lägger till fler hear me out:s, men det ä
 
 document.addEventListener("DOMContentLoaded", () => { 
     let hear_me_outs_storage = localStorage.getItem("hear-me-outs");
-    let hear_me_out_statuses = [];
-    // hear_me_outs_storage = localStorage.setItem("hear-me-outs", []);
+    let hear_me_out_statuses = {};
+    // hear_me_outs_storage = localStorage.setItem("hear-me-outs", {});
 
     if (hear_me_outs_storage) {
         hear_me_out_statuses = JSON.parse(hear_me_outs_storage);
     };
     console.log(hear_me_out_statuses);
 
-    for (let hear_me_out_number = 0; hear_me_out_number < document.getElementsByClassName("article").length; hear_me_out_number++) {
+    console.log(document.querySelectorAll("article.article"))
+
+    for (let hear_me_out_number = 0; hear_me_out_number < document.querySelectorAll("article.article").length; hear_me_out_number++) {
         const smash_button_element = document.getElementsByClassName("smash_button")[hear_me_out_number];
         const pass_button_element = document.getElementsByClassName("pass_button")[hear_me_out_number];
 
+        const specific_article_hear_me_out = document.querySelectorAll("article.article h2")[hear_me_out_number].innerHTML;
+        const specific_article_description = document.querySelectorAll("article.article p")[hear_me_out_number].innerHTML;
+        const specific_article_id = specific_article_hear_me_out + specific_article_description
+
         if (hear_me_outs_storage) {
-            if (hear_me_out_statuses[hear_me_out_number] == "Smash") {
+            if (hear_me_out_statuses[specific_article_id] == "Smash") {
                 smash_button_element.classList.add("clicked");
-            } else if (hear_me_out_statuses[hear_me_out_number] == "Pass") {
+            } else if (hear_me_out_statuses[specific_article_id] == "Pass") {
                 pass_button_element.classList.add("clicked");
             };
         } else {
-            hear_me_out_statuses[hear_me_out_number] = "None";
+            hear_me_out_statuses[specific_article_id] = "None";
         };
 
         smash_button_element.addEventListener("click", () => {
-            if (hear_me_out_statuses[hear_me_out_number] == "Smash") {
-                hear_me_out_statuses[hear_me_out_number] = "None";
+            if (hear_me_out_statuses[specific_article_id] == "Smash") {
+                hear_me_out_statuses[specific_article_id] = "None";
                 smash_button_element.classList.remove("clicked");
             } else {
-                hear_me_out_statuses[hear_me_out_number] = "Smash";
+                hear_me_out_statuses[specific_article_id] = "Smash";
                 smash_button_element.classList.add("clicked");
                 pass_button_element.classList.remove("clicked");
             };
@@ -42,11 +48,11 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(hear_me_out_statuses);
         });
         pass_button_element.addEventListener("click", () => {
-            if (hear_me_out_statuses[hear_me_out_number] == "Pass") {
-                hear_me_out_statuses[hear_me_out_number] = "None";
+            if (hear_me_out_statuses[specific_article_id] == "Pass") {
+                hear_me_out_statuses[specific_article_id] = "None";
                 pass_button_element.classList.remove("clicked");
             } else {
-                hear_me_out_statuses[hear_me_out_number] = "Pass";
+                hear_me_out_statuses[specific_article_id] = "Pass";
                 pass_button_element.classList.add("clicked");
                 smash_button_element.classList.remove("clicked");
             };
