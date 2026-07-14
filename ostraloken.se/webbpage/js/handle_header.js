a4fd3a1c8ade6d29296a8c410c4029f97539c6a5
trt1e <vilhelm.grill@gmail.com>
2026-07-14 17:28:05 +0200

    made scrollbar better

--- ostraloken.se/webbpage/js/handle_header.js
+++ ostraloken.se/webbpage/js/handle_header.js
@@ -1,5 +1,3 @@
-const header_element = document.querySelector("header");
-
 let scroll_position = 0;
 let old_scroll_position = 0;
 let position_diffrance = 0;
@@ -7,6 +5,7 @@ let positive_counter = 0; // how long you have scrolled up
 let negative_counter = 0; // how long you have scrolled down
 
 document.addEventListener("scroll", () => {
+    const header_element = document.querySelector("header");
     const scrollbar_element = document.querySelector("#scrollbar");
     
     scroll_position = window.scrollY;
@@ -19,16 +18,13 @@ document.addEventListener("scroll", () => {
         negative_counter = 0;
     };
     if (positive_counter >= 1) {
-        header_element.classList.remove("header_hidden");
-        // scrollbar_element.classList.add("scrollbar_hidden");
+        if (!header_element.classList.contains("header_during_scroll")) {
+            header_element.classList.remove("header_hidden");
+            console.log(header_element.classList.value)
+        }
     } else if (negative_counter >= 80) {
         header_element.classList.add("header_hidden");
     };
-    /*
-    if (negative_counter >= 180) {
-        scrollbar_element.classList.remove("scrollbar_hidden");
-    };
-    */
 
     old_scroll_position = scroll_position;
 });
--- ostraloken.se/webbpage/js/handle_scrollbar.js
+++ ostraloken.se/webbpage/js/handle_scrollbar.js
@@ -4,11 +4,13 @@
 const scrollbar_element = document.querySelector("#scrollbar");
 const scrollbar_area_element = document.querySelector("#scrollbar_area");
 
-document.addEventListener("DOMContentLoaded", () => { position_scrollbar() });
-document.addEventListener("scroll", () => { position_scrollbar() });
-document.addEventListener("resize", () => { position_scrollbar() });
+document.addEventListener("DOMContentLoaded", () => { position_scrollbar(); });
+document.addEventListener("scroll", () => { position_scrollbar(); });
+document.addEventListener("resize", () => { position_scrollbar(); });
 
 function position_scrollbar() {
+    scrollbar_element.classList.remove("scrollbar_hidden");
+
     // get where you are rn in the scroll
     const scroll_position = window.scrollY;
     // get the max scroll that you can scroll
@@ -24,40 +26,56 @@ function position_scrollbar() {
     console.log("Scrollbar height set to: " + wanted_scrollbar_height)
 };
 
+document.addEventListener("scrollend", () => { 
+    setTimeout( () => {
+        if (scrollbar_area_element.matches(":hover") == false) {
+            scrollbar_element.classList.add("scrollbar_hidden");
+        };     
+    }, 1000);
+});
+
 let move_mouse = false;
 let last_mouse_pos = 0;
 
-scrollbar_element.addEventListener("mousedown", () => { start_drag_scrollbar() });
-scrollbar_area_element.addEventListener("mouseup", () => { stop_drag_scrollbar() });
-scrollbar_area_element.addEventListener("mouseleave", () => { stop_drag_scrollbar() });
+scrollbar_element.addEventListener("mousedown", () => { start_drag_scrollbar(); });
+scrollbar_area_element.addEventListener("mouseup", () => { stop_drag_scrollbar(); });
+scrollbar_area_element.addEventListener("mouseleave", () => { 
+    stop_drag_scrollbar();
+    scrollbar_element.classList.add("scrollbar_hidden");
+});
 
-scrollbar_area_element.addEventListener("mousemove", (event) => { drag_scrollbar(event) });
+scrollbar_area_element.addEventListener("mousemove", (event) => { drag_scrollbar(event); });
+scrollbar_area_element.addEventListener("mouseover", () => {
+    scrollbar_element.classList.remove("scrollbar_hidden");
+});
 
 function start_drag_scrollbar() {
+    const header_element = document.querySelector("header");
     move_mouse = true;
+    header_element.classList.add("header_during_scroll");
 
     console.log("Started draging");
 };
 function stop_drag_scrollbar() {
+    const header_element = document.querySelector("header");
     move_mouse = false;
+    header_element.classList.remove("header_during_scroll");
 
     console.log("Stoped draging");
 };
 function drag_scrollbar(event) {
     if (move_mouse) {
-        // get where you are rn in the scroll
-        const scroll_position = window.scrollY;
+        scrollbar_element.classList.remove("scrollbar_hidden");
+
         // get the max scroll that you can scroll
         const scroll_limit = document.documentElement.offsetHeight - document.documentElement.clientHeight;
 
-        // get where the scrollbar is rn
-        const scrollbar_element_currant_y_position = scrollbar_element.offsetTop;
         // get where the mouse is rn
         const mouse_y_position = event.clientY;
 
         // Set the position where it should be
-        const wanted_y_position = ((mouse_y_position - last_mouse_pos) * (scroll_limit / document.documentElement.clientHeight)) + scroll_position;
+        const wanted_y_position = ((mouse_y_position / document.documentElement.clientHeight) * scroll_limit);
+        console.log(wanted_y_position)
         window.scrollTo(0, wanted_y_position);
     };
-    last_mouse_pos = event.clientY;
 };
