3d6a523adab849bf3d6926e41a5bd49b5b10ed37
trt1e <vilhelm.grill@gmail.com>
2026-09-22 17:46:27 +0200

    Korrekturlasare.ostraloken.se got a major update

--- generated/webb/korrekturlasare.ostraloken.se/webbsite/js/check_text.js
+++ generated/webb/korrekturlasare.ostraloken.se/webbsite/js/check_text.js
@@ -15,20 +15,112 @@ submitForm.addEventListener("submit", (event) => {
     const writerExtractedText = writerTextItem.value;
     const articleExtractedText = articleTextItem.value;
 
+    // Add the comments
+    addComments(titleExtractedText, texttypeExtractedText, writerExtractedText, articleExtractedText);
+
     // Fill in the template
-    fillTemplte(titleExtractedText, texttypeExtractedText, writerExtractedText, articleExtractedText)
+    fillTemplte(titleExtractedText, texttypeExtractedText, writerExtractedText, articleExtractedText);
+
+    // Export the txt-formated article
+    formatArticle(titleExtractedText, texttypeExtractedText, writerExtractedText, articleExtractedText);
 });
 
 
+// Check if the text flaggs anything wrong or out of order
+function videCheckText(text) {
+    let allComments = "";
+
+    // First we check if the right citationmarks are used
+    if (text.includes('"')) {
+        allComments += `NOTERA: " används, du bör använda ”\n`
+    };
+    if (text.includes('“')) {
+        allComments += `NOTERA: “ används, du bör använda ”\n`
+    };
+    if (text.includes("'")) {
+        allComments += `NOTERA: ' används, du bör använda ’\n`
+    };
+    if (text.includes("‘")) {
+        allComments += `NOTERA: ‘ används, du bör använda ’\n`
+    };
+
+    // We check so that there is no . before a citationmark
+    if (text.includes('."') || text.includes('.“') || text.includes('.”') || text.includes(".'") || text.includes(".‘") || text.includes(".’")) {
+        allComments += `NOTERA: . är placerad innan citattecken, så gör vi inte i svenskan utan den hamnar utanför\n`
+    };
+    
+    // We check so that there is no , before a citationmark
+    if (text.includes(',"') || text.includes(',“') || text.includes(',”') || text.includes(",'") || text.includes(",‘") || text.includes(",’")) {
+        allComments += `NOTERA: , är placerad innan citattecken, så gör vi inte i svenskan utan den hamnar utanför\n`
+    };
+
+    // Check if there is a "-" with both sides empty
+    if (text.includes(" - ")) {
+        allComments += `NOTERA: - används som ett –\n`
+    };
+
+    return allComments;
+};
+
+// Get the comment elements that are to be filled
+const titleCommentItem = document.querySelector("#output-rubrik");
+const texttypeCommentItem = document.querySelector("#output-texttyp");
+const writerCommentItem = document.querySelector("#output-skribent");
+const articleCommentItem = document.querySelector("#output-artikel");
+
+function addComments(title, texttype, writer, article) { // This adds the comments to the text
+    // Check the title:
+    let titleComment = videCheckText(title);
+    if (title.length > 100) {
+        titleComment +=  `NOTERA: Rubriken är ${title.length} karaktärer lång, och det är lite långt\n`
+    };
+
+    // Check the texttype:
+    let texttypeComment = videCheckText(texttype);
+    if (texttype.length > 22) {
+        texttypeComment +=  `NOTERA: Texttypen är ${texttype.length} karaktärer lång, och det är lite långt\n`
+    };
+    
+    // Check the writer:
+    let writerComment = videCheckText(writer);
+    if (writer.length > 30) {
+        writerComment +=  `NOTERA: Skribentens namn är ${writer.length} karaktärer lång, och det är lite långt\n`
+    };
+    
+    // Check the article:
+    let articleComment = videCheckText(article);
+    
+    // Then send the comments back
+    titleCommentItem.innerText = titleComment;
+    texttypeCommentItem.innerText = texttypeComment;
+    writerCommentItem.innerText = writerComment;
+    articleCommentItem.innerText = articleComment;
+};
+
+
 // Get the template elements that are to be filled
-const titleTextTemplateItem = document.querySelector("#title-template-container");
-const texttypeTextTemplateItem = document.querySelector("#texttype-template-container");
-const writerTextTemplateItem = document.querySelector("#writer-template-container");
-const articleTextTemplateItem = document.querySelector("#article-template-container");
+const titleTemplateItem = document.querySelector("#title-template-container");
+const texttypeTemplateItem = document.querySelector("#texttype-template-container");
+const writerTemplateItem = document.querySelector("#writer-template-container");
+const articleTemplateItem = document.querySelector("#article-template-container");
 
 function fillTemplte(title, texttype, writer, article) { // This fills in the template elements with their new content
-    titleTextTemplateItem.innerText = title;
-    texttypeTextTemplateItem.innerText = texttype;
-    writerTextTemplateItem.innerText = writer;
-    articleTextTemplateItem.innerText = article;
+    titleTemplateItem.innerText = title;
+    texttypeTemplateItem.innerText = texttype;
+    writerTemplateItem.innerText = writer;
+    articleTemplateItem.innerText = article;
+};
+
+
+// Get the format elements that are to be filled
+const wholeArticleFormatedItem = document.querySelector("#formated-article");
+
+function formatArticle(title, texttype, writer, article) { // This fills in the template elements with their new content
+    const outputText = `>>Rubrik: ${title}
+>>Texttyp: ${texttype}
+>>Skribent: ${writer}
+>>Artikel: 
+${article}`
+
+    wholeArticleFormatedItem.value = outputText;
 };
